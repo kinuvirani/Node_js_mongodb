a81c27cc4a1ee205b15f4c0a1acb6912c67e0e02
lcom118-one <kiran.virani1997@gmail.com>
2018-12-19 11:35:46 +0530

    Updated

--- playground/mongo_update.js
+++ playground/mongo_update.js
@@ -6,7 +6,7 @@ MongoClient.connect('mongodb://localhost:27017/student',(err,db)=>{
         return console.log("Unable to connect to database");
     }
     console.log("Connected to successfully");
-    db.collection('user').findOneAndUpdate({name:"Kinu"},{$set:{name:"Kiran"},$inc: { age: -1}},{returnOriginal:false}).then((res)=>{
+    db.collection('user').findOneAndUpdate({name:"Kiru"},{$set:{name:"Kiran"},$min:{age:20}},{returnOriginal:false}).then((res)=>{
         console.log(res);
     });
     db.close();
